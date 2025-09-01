8b96e33e14b351c7b4b9edfeb2fb6d4f5c4e9aea
Arindam1580-hash <cse22026@iiitkalyani.ac.in>
2025-09-01 14:37:59 +0530

    hi

--- backend/main.py
+++ backend/main.py
@@ -67,7 +67,7 @@ app = FastAPI(title="AI Resume Analyzer", description="Enhanced RAG system with 
 origins = [
     "http://localhost:5500",
     "http://127.0.0.1:5500",
-    # "https://mini-rag-sepia.vercel.app",
+    "https://mini-rag-five.vercel.app/",
     "http://localhost:3000",
     "http://127.0.0.1:3000"
 ]
